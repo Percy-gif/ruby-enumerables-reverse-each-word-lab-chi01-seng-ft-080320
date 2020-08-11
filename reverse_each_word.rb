1559e6b536f7577a5a9d0abf8f4c731493c84e50
Percy Major <chipercy@hotmail.com>
2020-08-11 00:18:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,26 @@
 string = "Hello there, and how are you?"
-new_string = []
+
+
 
 def reverse_each_word(string)
-  .each 
+  new_string = ""
+  #turn our string into an array 
+  
+  
+  
+  #string.each.reverse << new_string 
+  #string.each 
+end 
+
+######################################
+
+string = "Hello there, and how are you?"
+new_string = ""
+#turn our string into an array 
+
+def reverse_each_word(string)
+#string.collect.reverse << new_string
+#string.each do |str| 
+  puts str 
+  puts 
 end 
